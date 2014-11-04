0506d2de067aafddf8e1e122fbfa8f1dbcf74e5b
adityapratama <aditya.pratama.0010@gmail.com>
2014-11-04 16:32:39 +0700

    add spring data-rest dan data-jpa

--- src/main/java/com/aditya/pra/coba/Application.java
+++ src/main/java/com/aditya/pra/coba/Application.java
@@ -1,19 +1,21 @@
 package com.aditya.pra.coba;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class)
 @EnableAutoConfiguration
-@ComponentScan
 public class Application {
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
+		
+		/*ApplicationContext ctx = SpringApplication.run(Application.class, args);
 		
 		System.out.println("Let's inspect the beans provided by Spring Boot:");
 		
@@ -21,6 +23,6 @@ public class Application {
 		Arrays.sort(beanNames);
 		for(String beanName : beanNames){
 			System.out.println(beanName);
-		}
+		}*/
 	}
 }
